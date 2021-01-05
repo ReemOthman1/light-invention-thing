3b6b2bcb463ddcb498d3fb70afc47fd596b838a7
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-05 10:47:39 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+#while True:
+    #print("light level:"+ input.light_level())
+    #if input.light_level() < 7: 
+        #light.set_all(light.rgb(255, 255, 255))
+    #else:
+        #light.clear()
+
 while True:
-    print("light level:"+ input.light_level())
-    if input.light_level() < 7: 
-        light.set_all(light.rgb(255, 255, 255))
+    print("light level: " + input.light_level())
+    if input.light_level() >5:
+        light.set_all(light.rgb(0,0,0))
     else:
-        light.clear()
+        light.set_all(light.rgb(255,255,255))
+        light.set_brightness(100)
+        pause(3000)
--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
+// while True:
+// print("light level:"+ input.light_level())
+// if input.light_level() < 7: 
+// light.set_all(light.rgb(255, 255, 255))
+// else:
+// light.clear()
 while (true) {
-    console.log("light level:" + input.lightLevel())
-    if (input.lightLevel() < 7) {
-        light.setAll(light.rgb(255, 255, 255))
+    console.log("light level: " + input.lightLevel())
+    if (input.lightLevel() > 5) {
+        light.setAll(light.rgb(0, 0, 0))
     } else {
-        light.clear()
+        light.setAll(light.rgb(255, 255, 255))
+        light.setBrightness(100)
+        pause(3000)
     }
     
 }
